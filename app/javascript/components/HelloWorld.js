cdb2876c1c4d987dcca49388e2a43a23936450fc
adamjacobcruse@gmail.com <learn@learnT7.local>
2019-09-23 11:47:00 -0700

    added gems, react, helloworld component

---
+++ app/javascript/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class HelloWorld extends React.Component {
+  render () {
+    return (
+
+      <h1>HelloWorld Component</h1>
+
+    );
+  }
+}
+
+export default HelloWorld
